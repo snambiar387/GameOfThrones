96808d4aefb58190ec0bf2610c5bd544b7868fef
Sreehari M Nambiar <snambiar387@gmail.com>
2018-05-12 08:58:26 +0530

    Added files.

---
+++ GOT/GOT/Scenes/KingsList/KingsListInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  KingsListInteractor.swift
+//  GOT
+//
+//  Created by macadmin on 5/12/18.
+//  Copyright © 2018 macadmin. All rights reserved.
+//
+
+import Foundation
---
+++ GOT/GOT/Scenes/KingsList/KingsListPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  KingsListPresenter.swift
+//  GOT
+//
+//  Created by macadmin on 5/12/18.
+//  Copyright © 2018 macadmin. All rights reserved.
+//
+
+import Foundation
+
+//TODO: IMPLEMENT COPY-ON-WRITE
+
+struct KingViewModel {
+    
+}
